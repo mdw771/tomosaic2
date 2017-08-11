6c9f61a755bd344c7937db4db40150b6059f88d8
Ming <mingdu2015@u.northwestern.edu>
2017-08-11 14:43:50 -0500

    bug fix reorg

--- tomosaic/util/util.py
+++ tomosaic/util/util.py
@@ -331,9 +331,9 @@ def reorganize_dir(file_list, raw_ds=(2,4), dtype='float16', **kwargs):
                 n_frames = full_shape[0]
                 alloc_sets = allocate_mpi_subsets(n_frames, size)
                 for frame in alloc_sets[rank]:
-                    temp = raw[frame, :, :]
+                    temp = raw[frame:frame+1, :, :]
                     temp = image_downsample(temp, ds)
-                    dat[frame, :, :] = temp
+                    dat[frame:frame+1, :, :] = temp
                     print('\r    Rank: {:d}, DS: {:d}, at frame {:d}'.format(rank, ds, frame))
                 print(' ')
                 # downsample flat/dark field data
@@ -345,18 +345,18 @@ def reorganize_dir(file_list, raw_ds=(2,4), dtype='float16', **kwargs):
                 comm.Barrier()
                 print('    Downsampling whites and darks')
                 for frame in range(aux_shape[0]):
-                    temp = raw[frame, :, :]
+                    temp = raw[frame:frame+1, :, :]
                     temp = image_downsample(temp, ds)
-                    dat[frame, :, :] = temp
+                    dat[frame:frame+1, :, :] = temp
                 raw = o['exchange/data_dark']
                 aux_shape = raw.shape
                 dat = dat_grp.create_dataset('data_dark', (aux_shape[0], np.floor(aux_shape[1]/ds),
                                                            np.floor(aux_shape[2]/ds)), dtype=dtype)
                 comm.Barrier()
                 for frame in range(aux_shape[0]):
-                    temp = raw[frame, :, :]
+                    temp = raw[frame:frame+1, :, :]
                     temp = image_downsample(temp, ds)
-                    dat[frame, :, :] = temp
+                    dat[frame:frame+1, :, :] = temp
                 comm.Barrier()
                 f.close()
                 comm.Barrier()
