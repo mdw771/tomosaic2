2ea98029b0f5b7c3e867b4042c20aefb43e6cdf4
Ming <mingdu2015@u.northwestern.edu>
2017-08-15 09:50:59 -0500

    fix reorg

--- tomosaic/util/util.py
+++ tomosaic/util/util.py
@@ -277,10 +277,10 @@ def reorganize_dir(file_list, raw_ds=(2,4), dtype='float16', **kwargs):
 
     # downsample
     try:
-        f = h5py.File(file_list[0])
+        f = h5py.File(file_list[0], 'r')
         full_shape = f['exchange/data'].shape
     except:
-        f = h5py.File(os.path.join('data_raw_1x', file_list[0]))
+        f = h5py.File(os.path.join('data_raw_1x', file_list[0]), 'r')
         full_shape = f['exchange/data'].shape
     comm.Barrier()
 
